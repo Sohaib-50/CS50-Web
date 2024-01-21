ac519ba4ee0d266ffb139796c0bcdc9003301c21
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2024-01-21 19:46:36 +0500

    worked on editor style

--- cs50w_finalproject/tehreer/forms.py
+++ cs50w_finalproject/tehreer/forms.py
@@ -2,6 +2,22 @@ from django import forms
 from .models import Article
 
 class ArticleForm(forms.ModelForm):
+    
     class Meta:
         model = Article
         fields = ("title", "body")
+
+        # remove label for body field
+        labels = {
+            "body": "",
+            "title": ""
+        }
+
+        widgets = {
+            # large title input, label and box center
+            "title": forms.TextInput(attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Title",
+                "style": "text-align: center; border: none; border-bottom: 3px solid gray; border-radius: 0px; box-shadow: none; background-color: transparent; font-size: 2rem; font-weight: bold; color: #000000;"
+            })                
+        }
