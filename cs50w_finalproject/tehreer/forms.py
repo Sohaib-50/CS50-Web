f24c3adad36159fc8b04c7869dc462e2b54b94f8
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2024-02-20 13:11:28 +0500

    signup form ux work

--- cs50w_finalproject/tehreer/forms.py
+++ cs50w_finalproject/tehreer/forms.py
@@ -14,17 +14,28 @@ class SignupForm(forms.ModelForm):
         fields = ['email', 'password', 'password2', 'first_name', 'last_name', 'bio', 'profile_picture']
 
 
-    def clean_password1(self):
-        password1 = self.cleaned_data.get('password1')
-        if len(password1) < 4:
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if len(password) < 4:
             raise forms.ValidationError("Password must be at least 4 characters long.")
-        return password1
-
+        return password
+    
     def clean_password2(self):
         password2 = self.cleaned_data.get('password2')
         if len(password2) < 4:
             raise forms.ValidationError("Password must be at least 4 characters long.")
         return password2
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        self.validate_password_match(password, password2)
+
+    def validate_password_match(self, password, password2):
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Passwords do not match.")
     
     
 class ArticleForm(forms.ModelForm):
--- cs50w_finalproject/tehreer/views.py
+++ cs50w_finalproject/tehreer/views.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import User
@@ -12,24 +13,58 @@ def index(request):
     return render(request, 'tehreer/index.html')
 
 def auth(request):
-    auth_message = request.GET.get("auth_message")
-    focus_signup = request.GET.get("focus_signup", "false").lower()
+    auth_messages: List[str] = request.session.pop("auth_messages", [])
+    focus_signup: bool = request.session.pop("focus_signup", "false").lower()
+    signup_form: SignupForm = request.session.pop("signup_form", SignupForm())
+
+    # debugging
     focus_signup = 'true'
+    # auth_messages = ["Poor email sick dubste Poor email", "sick dubstep","Poor email", "sick dubstep", "Poor email", "sick dubstep","Poor email", "sick dubstep","Poor email", "sick dubstep"]
+    auth_messages = ["Enter a valid email address.", "Passwords do not match."]
+
 
     return render(request, 'tehreer/auth.html', {
-        "auth_message": auth_message,
+        "auth_messages": auth_messages,
         "focus_signup": True if focus_signup == 'true' else False,
+        "signup_form": signup_form
     })
 
 
 def signup(request):
-    form = SignupForm(request.POST)
-    # print(form)
-    print(form.is_valid())
-    for field, field_errors in form.errors.items():
-                for error in field_errors:
-                    print(f"{field.capitalize()}: {error}")
-    return HttpResponseRedirect(reverse("tehreer:auth"))
+    signup_form = SignupForm(request.POST)
+    print(signup_form.is_valid())
+
+    if not signup_form.is_valid():
+
+        # add error messages to auth_messages list in session for passing to auth view and displaying on frontend
+        for field, field_errors in signup_form.errors.items():
+            for error in field_errors:
+                request.session.setdefault("auth_messages", []).append(error)
+        request.session["focus_signup"] = "true"
+        request.session["signup_form"] = signup_form
+        return HttpResponseRedirect(reverse("tehreer:auth"))
+    
+    else:
+        request.session.setdefault("auth_messages", []).append("Successfully signed up!")
+        request.session["focus_signup"] = "true"
+        return HttpResponseRedirect(reverse("tehreer:auth"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     # email: str = request.POST.get("email")
